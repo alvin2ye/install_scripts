0049f0130692483d408334a9749cc18dd85d7587
alvin2ye <alvin.ye.cn@gmail.com>
2014-06-03 23:09:38 +0800

    fix gcc version support

--- install_rbenv_ree187.bash
+++ install_rbenv_ree187.bash
@@ -8,6 +8,10 @@ git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-buil
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.profile
 echo 'eval "$(rbenv init -)"' >> $HOME/.profile
 source $HOME/.profile
+
+# if your gcc version > 4.6 must run
+export CFLAGS="-O2 -fno-tree-dce -fno-optimize-sibling-calls"
+
 CONFIGURE_OPTS="--no-tcmalloc" rbenv install ree-1.8.7-2012.02
 rbenv rehash
 rbenv global ree-1.8.7-2012.02
