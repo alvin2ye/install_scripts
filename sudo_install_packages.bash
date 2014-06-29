43ca45be3718378770f40c82c0c4f242500904c3
Alvin Ye 叶冬开 <alvin.ye.cn@gmail.com>
2014-06-29 09:37:44 +0800

    Update sudo_install_packages.bash

--- sudo_install_packages.bash
+++ sudo_install_packages.bash
@@ -9,7 +9,7 @@ sudo apt-get -y install curl git-core python-software-properties
 sudo locale-gen zh_CN.UTF-8
 sudo dpkg-reconfigure locales
 sudo apt-get install -y git
-sudo apt-get install -y wget vim build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf automake libtool imagemagick libmagickwand-dev libpcre3-dev
+sudo apt-get install -y wget vim build-essential openssl libreadline libreadline-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf automake libtool imagemagick libmagickwand-dev libpcre3-dev
 sudo apt-get install -y libcurl4-openssl-dev
  
 # sqlite3
