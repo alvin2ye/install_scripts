aa729e6090e1021401b7a7093015e378dadd4560
Alvin <alvin@agideo.com>
2016-08-18 20:38:36 +0800

    chg docker-compose

--- ubuntu_14.04/install_docker.bash
+++ ubuntu_14.04/install_docker.bash
@@ -4,6 +4,8 @@ sudo apt-get install -y curl git
 curl -fsSL https://get.docker.com/ | sh
 curl -fsSL https://get.docker.com/gpg | sudo apt-key add -
 
-curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+# docker-compose
 
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
